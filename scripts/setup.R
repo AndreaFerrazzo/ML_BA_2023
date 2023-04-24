b174f3d95294689405e4075ec1dd77706bebf52a
jordanvazquez <127729476+jordanvazquez@users.noreply.github.com>
2023-04-24 14:55:49 +0200

    Added setup file for packages and report options

---
+++ scripts/setup.R
@@ -0,0 +1,32 @@
+# load the required packages
+packages <- c(
+  "here", "tidyverse", "summarytools", "janitor"
+)
+purrr::walk(packages, library, character.only = TRUE)
+
+
+#Report Options
+
+
+# general options
+options(
+  digits = 3,
+  str = strOptions(strict.width = "cut"),
+  width = 69,
+  tibble.width = 69,
+  cli.unicode = FALSE
+)
+
+# ggplot options
+theme_set(theme_light())
+theme_update(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))
+
+# knitr options
+opts_chunk$set(
+  comment = "#>",
+  message = FALSE,
+  warning = FALSE,
+  fig.align = "center",
+  echo = TRUE,
+  cache = TRUE
+)
